9576bfb1e0027ac281b78bdeffdd2238641ba4fe
lunoxx <a.mese99@gmail.com>
2022-11-18 20:07:52 +0200

    Update javascript section
    Now ,you can set names for Player 1 and Player 2

--- js/dice.js
+++ js/dice.js
@@ -1,5 +1,5 @@
-
 window.onload = function() {
+
     var reloading = sessionStorage.getItem("reloading");
 
     if (reloading) {
@@ -12,18 +12,26 @@ window.onload = function() {
         var randomNumber2 = 1 + Math.floor(Math.random() * 6);
         document.querySelector(".dice .img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
+        var winnerText = [], 
+            name1 = sessionStorage.getItem("player1"),
+            name2 = sessionStorage.getItem("player2");
 
-        var winnerText = [];
+        document.getElementsByTagName("p")[0].innerHTML = "Player 1: " + name1;
+        document.getElementsByTagName("p")[1].innerHTML = "Player 2: " + name2;
 
-        if(randomNumber1 > randomNumber2) winnerText = "&#128681 Player 1 Wins";
-        else if(randomNumber2 > randomNumber1) winnerText = "Player 2 Wins &#128681";
+        if(randomNumber1 > randomNumber2) winnerText = "&#128681 " + name1 + " Wins";
+        else if(randomNumber2 > randomNumber1) winnerText = name2 + " Wins &#128681";
         else winnerText = "Draw!";
 
         document.getElementsByTagName("h1")[0].innerHTML = winnerText;
     }
     else {
-        
+        var name1 = prompt("Please enter first player name:");
+        var name2 = prompt("Please enter second player name.");
+
         sessionStorage.setItem("reloading", true);
+        sessionStorage.setItem("player1", name1);
+        sessionStorage.setItem("player2", name2);
     }
 }
 
